b91591fd52727b301430ab1e673833f1bf1cad6a
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-01-23 12:40:57 +0530

    3rd commit

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (inputBox.value === "") {
+  if (inputBox.value === "") { // when if when user don't add any input which means, inputBox.value is empty
+    //when user click the add button without any value this alert msg will show.
     alert("You must write a task");
   } else {
+    //if user input the value and click the add button below code create the list container
     let li = document.createElement("li"); //It's like creating a new line on your paper for a new task.
+
+    //then it will add the task to the list
     li.innerHTML = inputBox.value; //It's like writing your task on the new line you created. The inputBox.value is what you typed in the input box, and you're putting that on your list.
+
+    //then it will show the task in the to do list container
     listContainer.appendChild(li); //It's like sticking the piece of paper with your new task onto your to-do list. Now, your list has one more task on it!
 
     let span = document.createElement("span");
